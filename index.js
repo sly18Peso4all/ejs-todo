62ceb6513374faa8515645600341158d5ee96f13
sly18Peso4all <jomasco18@gmail>
2021-08-04 00:24:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const items = [
+  "Write List item",
+  "Go to the Market",
+  " Buy item",
+  "Prepare item",
+  "Eat food",
+];
+
+const workItems = [];
+
+app.get("/", (req, res) => {
+  let day = date.getDate();
+
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.listen(3000, () => console.log("Application is running on port 3000"));
